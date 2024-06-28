15fb8a41850a8ca3dcc31e0fdbac163dbce04871
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-06-28 13:51:52 +0200

    Upd

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -1,16 +1,47 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 
 namespace CoreApp;
+
 public class Program
 {
-    
-    public static void Main(string[] args)
+    private static readonly HttpClient client = new();
+    private static IConfiguration _configuration;
+
+
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true);
+
+        _configuration = builder.Build();
+
+        client.BaseAddress = new Uri("https://youtrack.jetbrains.com/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        // TODO: move token to config
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+            "");
+
+        try
+        {
+            var issues = await GetIssuesAsync("ReSharper", 10);
+            foreach (var issue in issues) Console.WriteLine($"Issue {issue.Id}: {issue.Summary}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static async Task<List<YouTrackIssue>> GetIssuesAsync(string project, int limit)
+    {
+        var query = $"api/issues?fields=id,summary,description&query=project:{project}&$top={limit}";
+        var response = await client.GetAsync(query);
+        response.EnsureSuccessStatusCode();
+
+        var data = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<YouTrackIssue>>(data);
     }
 }
