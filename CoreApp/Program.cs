d4670fcde559a89940435e949b3855fd91f31c20
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-06-28 10:27:53 +0200

    Add project files.

---
+++ CoreApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CoreApp;
---
+++ CoreApp/YouTrackIssue.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreApp;
+
+public class YouTrackIssue
+{
+    public string Id { get; set; }
+    public string Summary { get; set; }
+    public string Description { get; set; }
+}
